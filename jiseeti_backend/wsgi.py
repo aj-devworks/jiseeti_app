e5394c7d38ba804a86526161993531a1f55d08a7
aj07256768-sys <aj07256768@gmail.com>
2026-09-03 12:19:00 +0300

    Auto-run migrations on startup for free-tier Render compatibility

--- jiseeti_backend/wsgi.py
+++ jiseeti_backend/wsgi.py
@@ -1,5 +1,9 @@
 import os
+from flask_migrate import upgrade
 from app import create_app
 
 env = os.getenv("FLASK_ENV", "production")
 app = create_app(env)
+
+with app.app_context():
+    upgrade()
